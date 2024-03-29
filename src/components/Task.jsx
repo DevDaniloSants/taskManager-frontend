7788fa8e9050da46b5d3a90fbb2757bb2e3f3356
Danilo Santos <dscofield@outlook.com.br>
2024-03-29 20:32:25 -0300

    feat: Add AddTask component to Task component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import './Task.scss';
 import TaskItem from './TaskItem';
+import AddTask from './AddTask';
 
 const Task = () => {
   const [tasks, setTasks] = useState([]);
@@ -27,6 +28,7 @@ const Task = () => {
 
       <div className="last-tasks">
         <h3>Últimas Tarefas</h3>
+        <AddTask />
         <div className="tasks-list">
           {tasks
             .filter((task) => !task.isCompleted)
