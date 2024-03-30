e22faae1274a4d999531aca945bc369f3d55e30e
Danilo Santos <dscofield@outlook.com.br>
2024-03-30 13:28:48 -0300

    feat: add function to add a task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,15 +1,42 @@
 import { useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 import CustomInput from './CustomInput';
+import CustomButton from './CustomButton';
+
+import { api } from '../utils/config';
 
 import './AddTask.scss';
 
-const AddTask = () => {
+const AddTask = ({ fetchTask }) => {
   const [task, setTask] = useState('');
 
   const handleChange = (e) => {
     setTask(e.target.value);
   };
+
+  const handleTaskAddition = async () => {
+    try {
+      if (task.length === 0) {
+        return toast.error('A tarefa precisa ter uma descrição');
+      }
+
+      await axios.post(api, {
+        description: task,
+        isCompleted: false,
+      });
+
+      fetchTask();
+      setTask('');
+
+      return toast.success('Tarefa adiciona com sucesso');
+    } catch (error) {
+      toast.error('Algo deu errado');
+    }
+  };
+
   return (
     <div className="add-task-container">
       <CustomInput
@@ -17,6 +44,9 @@ const AddTask = () => {
         value={task}
         onChange={handleChange}
       />
+      <CustomButton onClick={handleTaskAddition}>
+        <FaPlus />
+      </CustomButton>
     </div>
   );
 };
