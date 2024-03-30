0de4fb52489247af3be4c2bd8be548067f6f65e3
Danilo Santos <dscofield@outlook.com.br>
2024-03-30 14:50:48 -0300

    feat: add app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Sidebar from './components/Sidebar';
 import Task from './components/Task';
 
 const App = () => {
   return (
-    <>
+    <div className="app-container">
+      <Sidebar />
       <Task />
-    </>
+    </div>
   );
 };
 
